7bf4e2ecd980883a690c4e27af2ba3e82cc3ed86
ryotatsuka <ryota-tsukamoto@kke.co.jp>
2016-12-09 11:00:42 +0900

    read local json
    read local json

--- app/controllers/locks_controller.rb
+++ app/controllers/locks_controller.rb
@@ -1,4 +1,12 @@
 class LocksController < ApplicationController
   def show
+  	@msg = 'こんにちは。お久しぶりです。'
+  	json_file_path = 'app/assets/apiout_json/devices.json'
+  	#test = '{"data":"a","type":1,"attr":2}'
+    test = File.open(json_file_path,'r')
+  	json_data = ActiveSupport::JSON.decode(test.read)
+    
+  	@devices = json_data["data"]
+  	render "locks/show"
   end
 end
